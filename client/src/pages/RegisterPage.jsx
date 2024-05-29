7d386fc3c18c5ae952a1ccf89a70f48400969f13
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-05-29 18:35:04 -0300

    registerErrors

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -5,8 +5,12 @@ import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
-  const { register, handleSubmit } = useForm();
-  const { signup, isAuthenticated } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { signup, isAuthenticated, errors: registerErrors } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -20,12 +24,20 @@ const RegisterPage = () => {
   return (
     <div className={styles.registerContainer}>
       <form className={styles.registerForm} onSubmit={onSubmit}>
+        {registerErrors.map((error, i) => (
+          <div key={i} className={styles.registerError}>
+            {error}
+          </div>
+        ))}
         <input
           className={styles.registerInput}
           type="text"
           placeholder="User name"
           {...register("username", { required: true })}
         />
+        {errors.username && (
+          <p className={styles.registerError}>Username is required</p>
+        )}
 
         <input
           className={styles.registerInput}
@@ -33,6 +45,9 @@ const RegisterPage = () => {
           placeholder="E-mail"
           {...register("email", { required: true })}
         />
+        {errors.username && (
+          <p className={styles.registerError}>E-mail is required</p>
+        )}
 
         <input
           className={styles.registerInput}
@@ -40,6 +55,9 @@ const RegisterPage = () => {
           placeholder="Password"
           {...register("password", { required: true })}
         />
+        {errors.username && (
+          <p className={styles.registerError}>Password is required</p>
+        )}
 
         <button className={styles.registerButton} type="submit">
           Registrar

9d49a00f5b0c3dd92df5e2bb907b378a792d4512
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-06-07 11:34:32 -0300

    ProfilePage

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const ProfilePage = () => {
   return (
     <div>
-      Profile page
+      <h1>Profile Page is not allowed yet.</h1>
+      <h2>We are working on it c:</h2>
+      <h3>Username: Username</h3>
+      <h4>Tasks quantity: number</h4>
+      <h4>Account date created: date</h4>
+      <div>
+        <h2>Private settings</h2>
+        <h3>E-mail: E-mail | Edit</h3>
+        <h3>Password: ******* | Edit</h3>
+        <h3>Delete account</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage
-
+export default ProfilePage;

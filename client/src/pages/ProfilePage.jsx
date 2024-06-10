302e756857af83780fc6d319ec1e4fb905ffe1f7
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-06-10 12:02:42 -0300

    ProfilePage.jsx styles

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
+import styles from "./ProfilePage.module.css";
 
 const ProfilePage = () => {
   const { user, updateUser, errors, getUserTasksCount } = useAuth();
-  const [editMode, setEditMode] = useState({ username: false, email: false, password: false });
-  const [formData, setFormData] = useState({ username: user?.username, email: user?.email, password: "" });
+  const [editMode, setEditMode] = useState({
+    username: false,
+    email: false,
+    password: false,
+  });
+  const [formData, setFormData] = useState({
+    username: user?.username,
+    email: user?.email,
+    password: "",
+  });
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [tasksCount, setTasksCount] = useState(0);
@@ -27,9 +36,9 @@ const ProfilePage = () => {
 
   const handleSave = async (field) => {
     try {
-      const updatedData = {...formData}
+      const updatedData = { ...formData };
       if (!formData.password) {
-        delete updatedData.password
+        delete updatedData.password;
       }
 
       console.log("Updating user with data:", updatedData); // Log de datos enviados
@@ -44,70 +53,107 @@ const ProfilePage = () => {
   };
 
   return (
-    <div>
-      <h1>Profile Page is not allowed yet.</h1>
-      <h2>We are working on it c:</h2>
-      <h4>Tasks quantity: {tasksCount}</h4>
-      <h4>Account date created at: {user && new Date(user.createdAt).toLocaleDateString()}</h4>
+    <div className={styles.profilePageContainer}>
+      <h2>Profile values</h2>
+      <h4>
+        Tasks quantity: <span>{tasksCount}</span>
+      </h4>
+      <h4>
+        Account date created at:{" "}
+        <span>{user && new Date(user.createdAt).toLocaleDateString()}</span>
+      </h4>
       <div>
         <h2>Private settings</h2>
-        
+
         <h3>
-          Username: 
+          Username:
           {editMode.username ? (
             <>
-              <input 
-                type="text" 
-                name="username" 
-                value={formData.username} 
-                onChange={handleChange} 
+              <input
+                className={styles.editInput}
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
               />
-              <button onClick={() => handleSave('username')}>Save</button>
+              <button
+                className={styles.saveButton}
+                onClick={() => handleSave("username")}
+              >
+                Save
+              </button>
             </>
           ) : (
             <>
-              {user.username} 
-              <button onClick={() => handleEdit('username')}>Edit</button>
+              <span>{user.username}</span>
+              <button
+                className={styles.editButton}
+                onClick={() => handleEdit("username")}
+              >
+                Edit
+              </button>
             </>
           )}
         </h3>
-        
+
         <h3>
-          E-mail: 
+          E-mail:
           {editMode.email ? (
             <>
-              <input 
-                type="email" 
-                name="email" 
-                value={formData.email} 
-                onChange={handleChange} 
+              <input
+                className={styles.editInput}
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
               />
-              <button onClick={() => handleSave('email')}>Save</button>
+              <button
+                className={styles.saveButton}
+                onClick={() => handleSave("email")}
+              >
+                Save
+              </button>
             </>
           ) : (
             <>
-              {user.email} 
-              <button onClick={() => handleEdit('email')}>Edit</button>
+              <span>{user.email}</span>
+              <button
+                className={styles.editButton}
+                onClick={() => handleEdit("email")}
+              >
+                Edit
+              </button>
             </>
           )}
         </h3>
-        
+
         <h3>
-          Password: 
+          Password:
           {editMode.password ? (
             <>
-              <input 
-                type="password" 
-                name="password" 
-                value={formData.password} 
-                onChange={handleChange} 
+              <input
+                className={styles.editInput}
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
               />
-              <button onClick={() => handleSave('password')}>Save</button>
+              <button
+                className={styles.saveButton}
+                onClick={() => handleSave("password")}
+              >
+                Save
+              </button>
             </>
           ) : (
             <>
-              ***** 
-              <button onClick={() => handleEdit('password')}>Edit</button>
+              <span>*******</span>
+              <button
+                className={styles.editButton}
+                onClick={() => handleEdit("password")}
+              >
+                Edit
+              </button>
             </>
           )}
         </h3>
@@ -115,7 +161,7 @@ const ProfilePage = () => {
         {successMessage && <p>{successMessage}</p>}
         {errorMessage && <p>Error: {errorMessage}</p>}
         {errors.length > 0 && <p>Error: {errors.join(", ")}</p>}
-        
+
         <button>Delete account</button>
       </div>
     </div>

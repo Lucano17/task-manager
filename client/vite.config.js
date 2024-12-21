68106e2c69acf9e6e5ffcd4a18f77173a63b640c
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-12-21 17:21:43 -0300

    server file

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.BACKEND_SERVER_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })

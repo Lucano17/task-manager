457b3fef7dd06c232238e6d79195974d367a64da
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-12-21 22:49:27 -0300

    vite.config.js .env

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:4000",
+        target: process.env.BACKEND_SERVER_URL,
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),

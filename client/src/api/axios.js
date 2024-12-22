ee2670f9602d4a34539f5364e209ded5cbc076d2
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-12-21 22:51:48 -0300

    axios.js backend_server_url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_SERVER_URL || "http://localhost:4000/api";
+const BASE_URL = import.meta.env.BACKEND_SERVER_URL;
 
 const instance = axios.create({
     baseURL: BASE_URL,
